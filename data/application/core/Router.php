ac9159054c87f8b5dfdf412bfd7c58d0c2b33e9e
Nikolay Kvasov <kvasov1992@inbox.ru>
2020-03-12 21:03:48 +0400

    Первое видео

---
+++ data/application/core/Router.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace application\core;
+/**
+ * Class Router
+ * @package application\core
+ */
+class Router
+{
+    /**
+     * @var array
+     */
+    protected $routes = [];
+    /**
+     * @var array
+     */
+    protected $params = [];
+
+    /**
+     * Router constructor.
+     */
+    function __construct()
+    {
+        //
+    }
+
+    public function addRoute()
+    {
+        //
+    }
+
+    public function checkRoute()
+    {
+        //
+    }
+
+    public function runRoute()
+    {
+        echo 'Старт';
+    }
+}
--- data/index.php
+++ data/index.php
@@ -1,1 +1,24 @@
-<?php phpinfo() ?>
+<?php
+
+// Дебаг
+require 'application/libs/Dumper.php';
+
+use application\core\Router;
+
+/**
+ * @param $class
+ * Функция автозагрузчик классов
+ */
+spl_autoload_register(function ($class) {
+    $pathForClass = str_replace('\\', '/', $class . '.php');
+
+    if (file_exists($pathForClass)) {
+        require $pathForClass;
+    }
+});
+
+session_start();
+
+$router = new Router;
+$router->runRoute();
+
